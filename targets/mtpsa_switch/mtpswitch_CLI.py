04955b0ba7fee4c5f8a766ace40b2ec996c81951
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2020-04-01 07:46:48 +0100

    mtpswitch_CLI: Clean up
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- targets/mtpsa_switch/mtpswitch_CLI.py
+++ targets/mtpsa_switch/mtpswitch_CLI.py
@@ -13,12 +13,11 @@ class MtPsaSwitchAPI(runtime_CLI.RuntimeAPI):
         return [("mtpsa_switch", MtPsaSwitch.Client)]
 
     def __init__(self, pre_type, standard_client, mc_client, pswitch_client):
-        runtime_CLI.RuntimeAPI.__init__(self, pre_type,
-                                        standard_client, mc_client)
+        runtime_CLI.RuntimeAPI.__init__(self, pre_type, standard_client, mc_client)
         self.pswitch_client = pswitch_client
 
     def do_set_queue_depth(self, line):
-        "Set depth of one / all egress queue(s): set_queue_depth <nb_pkts> [<egress_port>]"
+        """Set depth of one / all egress queue(s): set_queue_depth <nb_pkts> [<egress_port>]"""
         args = line.split()
         depth = int(args[0])
         if len(args) > 1:
@@ -28,7 +27,7 @@ class MtPsaSwitchAPI(runtime_CLI.RuntimeAPI):
             self.pswitch_client.set_all_egress_queue_depths(depth)
 
     def do_set_queue_rate(self, line):
-        "Set rate of one / all egress queue(s): set_queue_rate <rate_pps> [<egress_port>]"
+        """Set rate of one / all egress queue(s): set_queue_rate <rate_pps> [<egress_port>]"""
         args = line.split()
         rate = int(args[0])
         if len(args) > 1:
@@ -38,38 +37,32 @@ class MtPsaSwitchAPI(runtime_CLI.RuntimeAPI):
             self.pswitch_client.set_all_egress_queue_rates(rate)
 
     def do_mirroring_add(self, line):
-        "Add mirroring mapping: mirroring_add <mirror_id> <egress_port>"
+        """Add mirroring mapping: mirroring_add <mirror_id> <egress_port>"""
         args = line.split()
         mirror_id, egress_port = int(args[0]), int(args[1])
         self.pswitch_client.mirroring_mapping_add(mirror_id, egress_port)
 
     def do_mirroring_delete(self, line):
-        "Delete mirroring mapping: mirroring_delete <mirror_id>"
+        """Delete mirroring mapping: mirroring_delete <mirror_id>"""
         mirror_id = int(line)
         self.pswitch_client.mirroring_mapping_delete(mirror_id)
 
     def do_get_time_elapsed(self, line):
-        "Get time elapsed (in microseconds) since the switch started: get_time_elapsed"
-        print self.pswitch_client.get_time_elapsed_us()
+        """Get time elapsed (in microseconds) since the switch started: get_time_elapsed"""
+        print(self.pswitch_client.get_time_elapsed_us())
 
     def do_get_time_since_epoch(self, line):
-        "Get time elapsed (in microseconds) since the switch clock's epoch: get_time_since_epoch"
-        print self.pswitch_client.get_time_since_epoch_us()
+        """Get time elapsed (in microseconds) since the switch clock's epoch: get_time_since_epoch"""
+        print(self.pswitch_client.get_time_since_epoch_us())
 
 def main():
     args = runtime_CLI.get_parser().parse_args()
-
     args.pre = runtime_CLI.PreType.SimplePreLAG
-
     services = runtime_CLI.RuntimeAPI.get_thrift_services(args.pre)
     services.extend(MtPsaSwitchAPI.get_thrift_services())
 
-    standard_client, mc_client, pswitch_client = runtime_CLI.thrift_connect(
-        args.thrift_ip, args.thrift_port, services
-    )
-
+    standard_client, mc_client, pswitch_client = runtime_CLI.thrift_connect(args.thrift_ip, args.thrift_port, services)
     runtime_CLI.load_json_config(standard_client, args.json)
-
     MtPsaSwitchAPI(args.pre, standard_client, mc_client, pswitch_client).cmdloop()
 
 if __name__ == '__main__':
